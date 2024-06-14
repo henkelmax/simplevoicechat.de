3106aa274d46ff1bee68a4d6c68aba8b8a846543
Max Henkel <max@maxhenkel.de>
2024-06-14 21:16:19 +0200

    Add tailwind

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <body class="bg-gray-100 flex items-center justify-center h-full w-full">
+  <div class="text-center">
+    <h1 class="text-4xl font-bold text-gray-600">Simple Voice Chat</h1>
+    <p class="text-gray-700 mt-4">This page is still under construction</p>
+  </div>
+  </body>
+</template>
